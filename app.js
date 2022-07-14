63b16a2386ba263d863842ac46d7210bd1615c34
Rose Pineau <rosepineau@protonmail.com>
2022-07-14 12:23:36 -0400

    Create array of divBoxes to access grid.

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // Create x16 divs on DOM load:
 var containerDiv = document.getElementById('grid-container');
+
 document.addEventListener('DOMContentLoaded', function() 
 {
     for(let i = 0; i < 256; i++)
@@ -7,14 +8,15 @@ document.addEventListener('DOMContentLoaded', function()
         var div = document.createElement('div');
         div.id = 'container';
         div.innerHTML = 'hi!';
-        div.className = 'border-pad';
-
         containerDiv.appendChild(div);
     }
-}, false);
-
-// Create pixelated hover event for ea div:
-document.addEventListener('mouseover', function()
-{
-    div.style.color = red;
 });
+
+console.log(containerDiv);
+// Create array of divBoxes created on DOM load:
+const divBoxes = containerDiv.childNodes;
+console.log(divBoxes);
+
+
+
+
