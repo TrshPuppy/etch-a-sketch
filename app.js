ddf44cab5069465aece59eab2cf383e96132d9e3
Rose Pineau <rosepineau@protonmail.com>
2022-07-14 10:52:19 -0400

    Add containerDiv and append divs to it.

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// Create x16 divs on DOM load:
+var containerDiv = document.getElementById('grid-container');
 document.addEventListener('DOMContentLoaded', function() 
 {
     for(let i = 0; i < 16; i++)
@@ -5,10 +7,8 @@ document.addEventListener('DOMContentLoaded', function()
         var div = document.createElement('div');
         div.id = 'container';
         div.innerHTML = 'hi!';
-        div.className= 'border pad';
+        div.className = 'border-pad';
 
-        document.body.appendChild(div);
+        containerDiv.appendChild(div);
     }
-    
-    
 }, false);
