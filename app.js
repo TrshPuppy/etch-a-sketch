a248b7b7bf5272fedb076f7a892c60a627025b78
Rose Pineau <rosepineau@protonmail.com>
2022-07-14 13:43:07 -0400

    Remove old code. Add comments.

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ canvas.style.gridTemplateRows = "repeat(16, 1fr)";
 for(let i = 0; i < 256; i++)
 {
     let pixel = document.createElement('div');
-    pixel.style.backgroundColor = "purple";
+    pixel.style.backgroundColor = "grey";
     canvas.insertAdjacentElement('beforeend', pixel);
 
     pixel.addEventListener('mouseover', () =>
@@ -16,52 +16,7 @@ for(let i = 0; i < 256; i++)
     });
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// var containerDiv = document.getElementById('grid-container');
-
-// document.addEventListener('DOMContentLoaded', function() 
-// {
-//     for(let i = 0; i < 256; i++)
-//     {
-//         var div = document.createElement('div');
-//         div.id = 'container';
-//         div.innerHTML = 'hi!';
-//         containerDiv.appendChild(div);
-//     }
-// });
-
-// // console.log(containerDiv);
-// // // Create nodelist of divBoxes created on DOM load:
-// // const divBoxes = containerDiv.childNodes;
-// // console.log(divBoxes);
-
-// // // Convert nodelist to array
-// // const DBOXESARRAY= Array.prototype.slice.call(divBoxes);
-// // console.log(DBOXESARRAY);
-
-// // // Add event listener to boxes:
-// // // divBoxes.forEach(dBox => 
-// // // {
-// // //     dBox.addEventListener('click', e =>
-// // //     {
-// // //         console.log('click');
-// // //         console.log(e);
-// // //     });
-// // // });
-
-
-
+// // Some things I need:
+// - canvas dimensions (input)
+// - color of pen (input/button)
+// - reset function (reset button)
