fe588ca4e3f7b0eacd16d9a8dae8cf6f2f8c52cc
Rose Pineau <rosepineau@protonmail.com>
2022-07-14 13:06:56 -0400

    Comment out and start over.

--- app.js
+++ app.js
@@ -1,22 +1,35 @@
-// Create x16 divs on DOM load:
-var containerDiv = document.getElementById('grid-container');
+// // Create x16 divs on DOM load:
+// var containerDiv = document.getElementById('grid-container');
 
-document.addEventListener('DOMContentLoaded', function() 
-{
-    for(let i = 0; i < 256; i++)
-    {
-        var div = document.createElement('div');
-        div.id = 'container';
-        div.innerHTML = 'hi!';
-        containerDiv.appendChild(div);
-    }
-});
+// document.addEventListener('DOMContentLoaded', function() 
+// {
+//     for(let i = 0; i < 256; i++)
+//     {
+//         var div = document.createElement('div');
+//         div.id = 'container';
+//         div.innerHTML = 'hi!';
+//         containerDiv.appendChild(div);
+//     }
+// });
 
-console.log(containerDiv);
-// Create array of divBoxes created on DOM load:
-const divBoxes = containerDiv.childNodes;
-console.log(divBoxes);
+// // console.log(containerDiv);
+// // // Create nodelist of divBoxes created on DOM load:
+// // const divBoxes = containerDiv.childNodes;
+// // console.log(divBoxes);
 
+// // // Convert nodelist to array
+// // const DBOXESARRAY= Array.prototype.slice.call(divBoxes);
+// // console.log(DBOXESARRAY);
+
+// // // Add event listener to boxes:
+// // // divBoxes.forEach(dBox => 
+// // // {
+// // //     dBox.addEventListener('click', e =>
+// // //     {
+// // //         console.log('click');
+// // //         console.log(e);
+// // //     });
+// // // });
 
 
 
